3144ff4738ee2b2792df1dabf662101c0b0c3803
Daniel Aragón <josedanielaragon@gmail.com>
2023-06-02 18:37:39 -0500

    homework javascript básico

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -168,16 +168,11 @@ function esPrimo(num) {
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
    
-   if (num <= 1) {
-      return false;
-    } 
-  
-    for (let i = 2; i < num; i++) {
-      if (num % i === 0) {
-        return false;
-      } 
-     return true; 
+   if (num <= 0 || num === 1) return false;
+    for (var i = 2; i < num; i++) {
+      if (num % i === 0) return false;
     }
+  return true;
 }
 
 function esVerdadero(valor) {
